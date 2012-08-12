f7cfd89969e4433de7c4ceaba5f60768bd15ad41
PaGrom <pagrom23@gmail.com>
2012-08-12 10:33:52 +0400

    fix

--- anti_screen_saver.py
+++ anti_screen_saver.py
@@ -1,8 +1,9 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 from pymouse import PyMouse
 import time
 
 while 1:
-	mouse.move(m.position()[0], m.position()[1]+1)
-	time.wait(60)
+	mouse = PyMouse()
+	mouse.move(mouse.position()[0], mouse.position()[1]+1)
+	time.sleep(60)
